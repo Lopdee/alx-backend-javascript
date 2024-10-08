fb891da0d23d4d6de4af8f0248587a203619384b
lopdee <ugochukwulopezubani@gmail.com>
2024-10-08 23:09:16 +0100

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+
+rt default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  return [...set].filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+}
